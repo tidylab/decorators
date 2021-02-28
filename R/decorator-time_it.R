6b727a4193c17399bf98942facce23628475eafe
harell <harel@loostiger.com>
2021-02-28 20:30:20 +1300

    add parameters to time_it

--- R/decorator-time_it.R
+++ R/decorator-time_it.R
@@ -1,6 +1,11 @@
 #' @title Measure Execution Time of Functions
 #' @description Wrap a function with a timer.
 #' @param func (`function`) A function.
+#' @param units (`character`) Units in which the results are desired, including:
+#'   \code{"auto"}, \code{"secs"}, \code{"mins"}, \code{"hours"}, \code{"days"},
+#'   and \code{"weeks"}.
+#' @param digits (`integer`) The number of significant digits to be used. See
+#'   \link[base]{signif}.
 #' @return (`closure`) An object that contains the original function bound to the environment of the decorator.
 #' @references
 #' * \href{https://docs.python.org/3/library/timeit.html}{timeit Python module}
@@ -10,13 +15,13 @@
 #' @examples
 #' Sys.sleep <- time_it(base::Sys.sleep)
 #' Sys.sleep(0.01)
-time_it <- function(func){
+time_it <- function(func, units = "auto", digits = 2){
     wrapper <- function(...){
         start_time <- Sys.time()
         output <- withVisible(func(...))
         end_time <- Sys.time()
 
-        print(difftime(end_time, start_time))
+        print(signif(difftime(end_time, start_time, units = units)), digits = digits)
         ifelse(output$visible, return(output$value), return(invisible(output$value)))
     }
 
